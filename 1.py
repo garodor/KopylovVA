7909306ac0462438361583a808af70a06beea7eb
garodor <90151484+garodor@users.noreply.github.com>
2021-11-25 21:00:49 +0300

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Введите число a "))
-b = int(input("Введите число b "))
-
-if a > b:
-    for i in range(a,b - 1,-2):
-        print(i)
-else:
-    for i in range(a,b + 1, 3):
-        print(i)
